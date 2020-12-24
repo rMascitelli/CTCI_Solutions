d53548db59aebb9ed2c219e99562503fe385101b
rMascitelli <robertmascitelli@gmail.com>
2020-12-24 12:44:05 -0500

    Finished up to 1.6

--- 1-Arrays_and_Strings.py
+++ 1-Arrays_and_Strings.py
@@ -52,4 +52,90 @@ def URLify(input_str, length):
 
 	return url[0:len(url)-3]
 
-print(URLify("testa ton rest     ", 15))
+# 1.4 - O(n)
+def palindromePermutation(input_str):
+
+	# Form a hits[] hash_table as I usually do
+	hits = [0] * 26
+	base = ord('a')
+	odd = (len(input_str) % 2) != 0			
+
+	for c in input_str:
+		if(c != ' '):
+			hits[ord(c.lower()) - base] += 1
+
+	print(hits)
+
+	# A palindrome needs an even # of chars
+	# And an odd number of one char if it's len() is odd (more than one odd hits[] means False)
+	num_odd = 0
+	for h in hits:
+		if((h%2) != 0):
+			if(num_odd > 0):
+				return False
+			else:
+				num_odd += 1
+
+
+	return True
+
+# 1.5 - O(n) worst case, O(1) best case
+def oneAway(str_a, str_b):
+
+	hits_a = [0] * 26; hits_b = [0] * 26
+	base = ord('a')
+	penalty = 0
+
+	# More than 2 difference in length is a short-circuit failure
+	if(abs(len(str_a) - len(str_b)) > 1):
+		return False
+
+	# Make the hits arrays
+	for str_x in [str_a, str_b]:
+		for c in str_x:
+			if(str_x == str_a):
+				hits_a[ord(c) - base] += 1
+			else:
+				hits_b[ord(c) - base] += 1
+
+	# "oneAway" pairs should have max ONE mismatch in number of each character
+	for i in range(0, 26):
+		if(hits_a[i] != hits_b[i]):			
+			if(penalty < 1):
+				# Multiple characters
+				if(abs(hits_a[i] - hits_b[i]) > 1):
+					return False
+				else:
+					penalty += 1
+			
+			else:
+				if(len(str_a) != len(str_b)):
+					return False
+	
+	# One letter difference
+	if(penalty < 2):
+		return True
+	else:
+		return False
+
+# 1.6
+def stringCompress(input_str):
+
+	last_char = ''
+	ret_str = ""
+	span = 0
+	for i in range(0, len(input_str)):
+		if(input_str[i] != last_char):
+			if(i > 0):
+				ret_str += str(last_char) + str(span)
+
+			last_char = input_str[i]
+			span = 1
+
+		else:
+			span += 1
+
+	ret_str += str(last_char) + str(span)
+	return ret_str
+
+print(stringCompress("aabcccccaaad"))
