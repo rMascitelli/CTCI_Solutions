f31a99b51b2ea75e9074350d22cdb5e27754c253
rMascitelli <robertmascitelli@gmail.com>
2020-12-24 17:32:15 -0500

    Finished Chapter 1... review Matrix operation/manipulation

--- 1-Arrays_and_Strings.py
+++ 1-Arrays_and_Strings.py
@@ -1,5 +1,16 @@
 # Chapter 1 - Arrays and Strings
 
+A = [[1, 2, 3, 4],
+     [5, 6, 0, 8], 
+     [9, 10, 11, 12], 
+     [13, 3, 15, 16],
+     [9, 17, 9, 14]]
+
+# Print a matrix, formatted nicely
+def printMatrix(A):
+	print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in A]))
+
 # 1.1 - O(n)
 def isUnique(input_str):
 	hits = [''] * 26
@@ -118,7 +129,7 @@ def oneAway(str_a, str_b):
 	else:
 		return False
 
-# 1.6
+# 1.6 - O(n)
 def stringCompress(input_str):
 
 	last_char = ''
@@ -138,4 +149,85 @@ def stringCompress(input_str):
 	ret_str += str(last_char) + str(span)
 	return ret_str
 
-print(stringCompress("aabcccccaaad"))
+# 1.7 - O(n^2)
+# TODO: Found this solution online, need to work on finding patterns
+# Also struggled with the looping indeces for this one
+def rotateMatrix(A):
+	"""
+        :type A: List[int][int]
+        List[x] gives the xth row
+    """
+
+	N = len(A[0])
+
+	for i in range(N // 2):
+		N = N-1
+		for j in range(i, N - i):
+			temp = A[i][j]
+			A[i][j] = A[N - j][i]
+			A[N - j][i] = A[N - i][N - j]
+			A[N - i][N - j] = A[j][N - i]
+			A[j][N - i] = temp
+
+# 1.8 - O(n^2)
+# TODO: is BCR = O(n^2) because we need to check every index? 
+def zeroMatrix(A):
+	"""
+		:type A: List[int][int]
+		If one element is 0, the whole row + column should be 0
+		For an m x n matrix
+	"""
+
+	N = len(A)
+	i_zeros = [0] * len(A)				# Hold position of zeros
+	j_zeros = [0] * len(A[0])			# Format like a 1:1 hash-map for O(1) lookup time
+
+	for i in range(0, N):
+		for j in range(0, len(A[0])):
+			if(A[i][j] == 0):
+				j_zeros[j] = j
+				i_zeros[i] = i
+
+	for i in range(0, N):
+		for j in range(0, len(A[0])):
+			if(j_zeros[j] != 0 or i_zeros[i] != 0):
+				A[i][j] = 0
+
+# 1.9 - O(n)
+def stringRotation(str_a, str_b):
+
+	if(str_a == str_b):
+		return True
+	# They must be the same length
+	elif(len(str_a) != len(str_b)):
+		return False
+
+	start = None
+	found_start = False
+
+	# We need to identify the "rotation point"
+	for i in range(0, len(str_b)):
+
+		# Loop through, find str_a[0], and loop until the whole string is found
+		if(str_b[i] == str_a[0]):
+			#print("Match:", i)
+			# Once a match is found, loop through the rest of the string to make sure the rotation point is correct
+			for j in range(0, len(str_b)):
+				pos = i+j
+				if(pos > len(str_b)-1):
+					pos = 0 + (pos - len(str_b))
+				
+				#print(str_b[pos], " =", str_a[j], " ? ")
+				if(str_b[pos] != str_a[j]):
+					break
+
+				# If we get to the last element, set the start point
+				if(j == len(str_b)-1):
+					return True
+
+	return False
+
+print("Res =", stringRotation("taterbottlesx", "bottlesxtater"))
+
+
+
